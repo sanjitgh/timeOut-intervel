9e76f05b0ac797770814538239b5334871ba491b
Sanjit <sanjitkumarghosh0@gmail.com>
2024-10-05 00:28:01 +0600

    push

---
+++ p.js
@@ -0,0 +1,34 @@
+// setTimeout(()=>{
+//     console.log('Hello chandu')
+// },5000)
+
+
+
+
+
+
+// function delayedGretting(name, time) {
+//     setTimeout(() => {
+//         console.log('Hello, ' + name);
+//     }, time)
+// }
+// delayedGretting('Alice', 2000)
+
+
+
+
+
+// function tellJoke(a) {
+//     let time = 0;
+//     const setInt = setInterval(() => {
+//         time += a;
+//         console.log('jokes is here!')
+//         if (time >= 10) {
+//             clearInterval(setInt)
+//         }
+//     }, 1000)
+// }
+
+// tellJoke(1)
+
+
